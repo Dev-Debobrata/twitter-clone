753f1512059c0e00f6f74c8e3982cdc86579daa8
Dev-Debobrata <dev.debobratapal@gmail.com>
2022-07-03 23:46:54 +0530

    added Comment datatype

--- typings.d.ts
+++ typings.d.ts
@@ -1,15 +1,28 @@
-export interface Tweet extends TweetBody {
+export interface Tweet {
   _id: string
   _createdAt: string
   _updatedAt: string
   _rev: string
   _type: 'tweet'
   blockTweet: boolean
+  text: string
+  username: string
+  profileImage: string
+  tweetImage: string
 }
 
-export const TweetBody = {
-  text: string,
-  username: string,
-  profileImage: string,
-  tweetImage: string
+export interface Comment {
+  _id: string
+  _createdAt: string
+  _updatedAt: string
+  _rev: string
+  _type: 'comment'
+  comment: string
+  tweetId: string
+  username: string
+  profileImg: string
+  tweet: {
+    _ref: string
+    _type: 'reference'
+  }
 }
